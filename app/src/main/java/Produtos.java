b468670172b5957d149fa6f9a0ca91b400b96b95
Rai <queroqueijo765@gmail.com>
2024-04-18 16:51:28 -0300

    aula dia 18/4

---
+++ app/src/main/java/Produtos.java
@@ -0,0 +1,56 @@
+public class Produtos {
+    private String nome;
+    private double preco;
+
+    public Produtos(String nome, double preco, String descricao, int quantidade, String status) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.status = status;
+    }
+
+    private String descricao;
+    private int quantidade;
+    private String status;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
--- app/src/main/java/com/example/lugaluga/MainActivity.java
+++ app/src/main/java/com/example/lugaluga/MainActivity.java
@@ -3,13 +3,41 @@ package com.example.lugaluga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listCidade;
+    private String[] cidades = {
+            "São Carlos", "Porto Ferreira", "Descalvado", "Franca", "Rio Claro", "Bauru", "Itaubaté", "Ribs", "Jabu", "Brotas"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Isso é um teste
+
+
+        listCidade = findViewById(R.id.listaNomes);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+
+                getApplicationContext(),
+                android.R.layout.simple_expandable_list_item_1,
+                android.R.id.text1,
+                cidades
+        );
+
+        listCidade.setAdapter(adapter);
+
+        @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemSelecionado = listCidade.getItemAtPosition(position).tostring();
+                toast.makeText(getApplicationContext(), itemSelecionado, Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
